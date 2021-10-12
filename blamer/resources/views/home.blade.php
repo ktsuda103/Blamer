35101cebbf66cc4621512a581801522aaf3c966d
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-13 08:06:21 +0900

    update view home
    トップページのフォーマットを作成

--- blamer/resources/views/home.blade.php
+++ blamer/resources/views/home.blade.php
@@ -3,6 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <div class="col-12">
+            <div class="text-center">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h1>Blamer</h1>
+                        <p>スーパーやコンビニでついつい余計なものを買ってしまう。あなたにはこんな経験ありませんか？</p>
+                        <p>Blamerは意志の弱いあなたをサポートするアプリです。</p>
+                        <p>早速投稿してみましょう！</p>
+                        <p>チュートリアルは<a href="#">こちら</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header">{{ __('タイトル') }}</div>
