fecf1e9c7d11aed9cd7f1f035f1bd2e5398469bf
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-14 07:25:42 +0900

    add validate
    投稿画面のバリデーション実装

--- blamer/app/Http/Controllers/PostController.php
+++ blamer/app/Http/Controllers/PostController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostFormRequest;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('home');
@@ -14,5 +20,11 @@ class PostController extends Controller
     public function create()
     {
         return view('post/create');
+    }
+
+    public function store(PostFormRequest $request)
+    {
+        $data = $request->input();
+        dd($data);
     }
 }
--- blamer/resources/views/post/create.blade.php
+++ blamer/resources/views/post/create.blade.php
@@ -9,17 +9,21 @@
                     投稿画面
                 </div>
                 <div class="card-body">
+                @if(count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                     <form method="post" action="{{ route('post/store') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row">
                             <label for="title" class="col-md-3 col-form-label text-md-right">タイトル</label>
                             <div class="col-md-7">
                                 <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}">
-                                @if($errors->has('title'))
-                                    <div class="error">
-                                        <p>{{ $errors->first('title') }}</p>
-                                    </div>
-                                @endif
                             </div>
                         </div>
                         <div class="form-group row">
@@ -27,22 +31,12 @@
                             <div class="custom-file col-md-7">
                                 <input type="file" name="image" id="image" class="custom-file-input">
                                 <label for="image" class="custom-file-label">選択してください</label>
-                                @if($errors->has('image'))
-                                    <div class="error">
-                                        <p>{{ $errors->first('image') }}</p>
-                                    </div>
-                                @endif
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="comment" class="col-md-3 col-form-label text-md-right">投稿内容</label>
                             <div class="col-md-7">
                                 <textarea rows="10" cols="50" name="comment" id="comment" class="form-control" value="{{ old('comment') }}"></textarea>
-                                @if($errors->has('comment'))
-                                    <div class="error">
-                                        <p>{{ $errors->first('comment') }}</p>
-                                    </div>
-                                @endif
                             </div>
                         </div>
                         <div class="form-group row">
