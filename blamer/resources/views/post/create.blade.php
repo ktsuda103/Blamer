67fe38acae40c8039cd64c439138fcd4b5e4e009
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-14 06:58:21 +0900

    create formrequest
    フォームリクエストでバリデーション処理

--- blamer/resources/views/post/create.blade.php
+++ blamer/resources/views/post/create.blade.php
@@ -9,12 +9,17 @@
                     投稿画面
                 </div>
                 <div class="card-body">
-                    <form method="post" action="" enctype="multipart/form-data">
+                    <form method="post" action="{{ route('post/store') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row">
                             <label for="title" class="col-md-3 col-form-label text-md-right">タイトル</label>
                             <div class="col-md-7">
                                 <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}">
+                                @if($errors->has('title'))
+                                    <div class="error">
+                                        <p>{{ $errors->first('title') }}</p>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                         <div class="form-group row">
@@ -22,12 +27,22 @@
                             <div class="custom-file col-md-7">
                                 <input type="file" name="image" id="image" class="custom-file-input">
                                 <label for="image" class="custom-file-label">選択してください</label>
+                                @if($errors->has('image'))
+                                    <div class="error">
+                                        <p>{{ $errors->first('image') }}</p>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="comment" class="col-md-3 col-form-label text-md-right">投稿内容</label>
                             <div class="col-md-7">
                                 <textarea rows="10" cols="50" name="comment" id="comment" class="form-control" value="{{ old('comment') }}"></textarea>
+                                @if($errors->has('comment'))
+                                    <div class="error">
+                                        <p>{{ $errors->first('comment') }}</p>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                         <div class="form-group row">
