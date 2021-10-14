efabc39426060dc170849a50adc2502029f7ac3f
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-15 06:46:00 +0900

    投稿詳細画面を作成

--- blamer/resources/views/post/detail.blade.php
+++ blamer/resources/views/post/detail.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
+            <div class="card mb-5">
                 <div class="card-header">
                     {{ $post->title }}
                     <div class="text-right">
@@ -20,6 +20,33 @@
                 <div class="card-footer">
                     <p class="large">投稿内容</p>
                     {{ $post->comment }}
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            投稿者：
+                        </div>
+                        <div class="offset-md-4 col-md-4">
+                            投稿日時：
+                        </div>
+                        <div class="col-12">
+                            コメント
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <form method="post" action="">
+                        @csrf
+                        <div class="form-group">
+                            <label for="comment">コメント</label>
+                            <textarea name="comment" id="comment" cols="30" rows="10" class="form-control"></textarea>
+                            <input type="submit" value="送信" class="btn btn-primary">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
