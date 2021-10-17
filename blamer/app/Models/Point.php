ac0c3192e5a8630ad89097d645628a91ac154a54
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-17 14:22:56 +0900

    ポイント加算メソッドを作成

--- blamer/app/Models/Point.php
+++ blamer/app/Models/Point.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Point extends Model
 {
     use HasFactory;
+
+    /**
+     * ポイントを加算
+     * $param $user_id,$point
+     * 
+     */
+    public function insert_point($user_id,$point)
+    {
+        $this->user_id = $user_id;
+        $this->point = $point;
+        $this->save();
+    }
 }
