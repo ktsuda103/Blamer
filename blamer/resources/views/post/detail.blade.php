36960d45c9e88211e2e852a4d52ced39dbe436ef
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-17 09:55:00 +0900

    自分のコメントはベストコメントに選べないよう変更

--- blamer/resources/views/post/detail.blade.php
+++ blamer/resources/views/post/detail.blade.php
@@ -71,7 +71,7 @@
                             </div>
                             <div class="col-md-8 comment">{{ $comment->comment }}</div>
                             <div class="col-md-4">
-                                @if($post->user_id === \Auth::id())
+                                @if($post->user_id === \Auth::id() && $comment->user_id !== \Auth::id())
                                 <form method="post" action="{{ route('best_comment/store') }}" class="form-inline">
                                     @csrf
                                     <input type="hidden" name="post_id" value="{{ $post->id }}">
