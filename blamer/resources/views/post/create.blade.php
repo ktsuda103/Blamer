c712423189e9f10de0a1bf0ca402bc56fbc2d30b
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-20 06:47:40 +0900

    投稿画面を修正

--- blamer/resources/views/post/create.blade.php
+++ blamer/resources/views/post/create.blade.php
@@ -3,10 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-8">
+        <div class="col-10">
+            <div class="card heading">
+                <div class="card-body text-center sentense">
+                    <p><i class="fas fa-camera icon"></i>レシートを写真に撮って投稿してみましょう！</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-10">
             <div class="card">
                 <div class="card-header">
-                    投稿画面
+                    投稿フォーム
                 </div>
                 <div class="card-body">
                 @if(session('success'))
@@ -33,13 +40,12 @@
                         </div>
                         <div class="form-group row">
                             <label for="image" class="col-md-3 text-md-right">画像</label>
-                            <div class="custom-file col-md-7">
-                                <input type="file" name="image" id="image" class="custom-file-input">
-                                <label for="image" class="custom-file-label">選択してください</label>
+                            <div class="col-md-7">
+                                <input type="file" name="image" id="image" class="form-control post-image">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="comment" class="col-md-3 col-form-label text-md-right">投稿内容</label>
+                            <label for="comment" class="col-md-3 col-form-label text-md-right">ひとこと</label>
                             <div class="col-md-7">
                                 <textarea rows="10" cols="50" name="comment" id="comment" class="form-control" value="{{ old('comment') }}"></textarea>
                             </div>
