5f8885896d1e05c6b1206c7f4685b6f3c22da40a
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-21 22:13:29 +0900

    ポイント制度説明画面を作成

---
+++ blamer/resources/views/rank.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <div class="card heading">
+                <div class="card-body text-center sentense">
+                    <p><i class="fas fa-camera icon"></i>レシートを写真に撮って投稿してみましょう！</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
