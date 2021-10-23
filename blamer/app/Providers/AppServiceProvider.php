a7f06e1dab2ca9dcdd12dfeadd9db39337981bc3
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-10-23 22:24:43 +0900

    update AppServiceProvider
    プロバイダーを更新

--- blamer/app/Providers/AppServiceProvider.php
+++ blamer/app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Paginator::useBootstrap();
 
         view()->composer('*', function($view){
